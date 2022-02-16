767b9d30df49fb9372f0a32c2f4b69c974ac9f6f
Valeria-Montisci <valeria_montisci@hotmail.com>
2022-02-17 07:27:04 +1100

    added link to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
   Weather App
+  <footer>
+   Coded by {""}
+   <a href='https://tender-hoover-502b0b.netlify.app/' target="_blank">Valeria Montisci</a> 
+   {""} open-sourced on {""}
+   <a href='https://github.com/Valeria-Montisci/react-weather-app' target="_blank>">Github</a> 
+  </footer>
     </div>
   );
 }
